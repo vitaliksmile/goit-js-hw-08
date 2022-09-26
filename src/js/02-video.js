788ef2b60aa4933775806809f8d2af4327e60e81
vitaliksmile <vitaliksmile777@gmail.com>
2022-09-26 19:39:46 +0300

    02-video-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const videoPlayer = document.querySelector('iframe');
+const player = new Player(videoPlayer);
+// const onPlay = function (data) {
+//   // data is an object containing properties specific to that event
+// };
 
+const onPlay = function ({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
