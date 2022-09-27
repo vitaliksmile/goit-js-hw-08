8cfd291ec10f73cc793cadabf42b5e002c2d624b
vitaliksmile <vitaliksmile777@gmail.com>
2022-09-27 15:06:49 +0300

    Добавлення перевірки чи шось є в localStorage

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,14 +2,36 @@ import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 const videoPlayer = document.querySelector('iframe');
 const player = new Player(videoPlayer);
-// const onPlay = function (data) {
-//   // data is an object containing properties specific to that event
-// };
+const STORAGE_KEY = 'videoplayer-current-time';
 
-const onPlay = function ({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
-};
-
+setCurrentTime();
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+function onPlay({ seconds }) {
+  save(STORAGE_KEY, seconds);
+}
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+function setCurrentTime() {
+  if (!load(STORAGE_KEY)) {
+    return;
+  }
+
+  player.setCurrentTime(load(STORAGE_KEY));
+}
